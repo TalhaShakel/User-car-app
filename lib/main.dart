4c3fcf9f4dafd5742ba1c390b447c21ebe01cfbe
TalhaShakel <shakeeltalha637@gmail.com>
2022-11-08 18:58:55 +0500

    Started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:users_app/infoHandler/app_info.dart';
@@ -9,6 +10,9 @@ import 'package:users_app/splashScreen/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  // Stripe.publishableKey = 'your publish key';
+
+  // await Stripe.instance.applySettings();
   runApp(
     MyApp(
       child: ChangeNotifierProvider(
